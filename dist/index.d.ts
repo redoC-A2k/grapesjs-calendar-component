664cdddd94c7718b92e276a88bd814529be5afd8
afshan ahmed khan <afshanahmeda2k@gmail.com>
2024-01-27 23:32:05 +0530

    Removed dist from .gitignore

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+import { Plugin } from 'grapesjs';
+
+export type PluginOptions = {};
+declare const CalendarPlugin: Plugin<PluginOptions>;
+
+export {
+	CalendarPlugin as default,
+};
+
+export {};
